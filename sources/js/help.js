2d3279493664e1e18292954f4aaa2625918219c2
gerentwickler <132832396+gerentwickler@users.noreply.github.com>
2023-05-13 17:59:50 +0500

    PayPal Payment integration #3

--- sources/js/help.js
+++ sources/js/help.js
@@ -1,8 +1,17 @@
 paypal.Buttons({
   style: {
     layout: "vertical",
-    color:  "blue",
-    shape:  "rect",
-    label:  "paypal"
+    color: "blue",
+    shape: "rect",
+    label: "paypal"
+  },
+  createOrder: function (data, actions) {
+    return actions.order.create({
+      purchase_units: [{
+        amount: {
+          value: "0.99"
+        }
+      }]
+    });
   }
 }).render("#application");
